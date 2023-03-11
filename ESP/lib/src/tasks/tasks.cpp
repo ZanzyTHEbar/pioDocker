8d0d330ef04d9f51b11b00a6bb797e77b4e74036
DaOfficialWizard <45744329+ZanzyTHEbar@users.noreply.github.com>
2023-03-11 23:29:46 +0000

    Delete ESP directory

--- ESP/lib/src/tasks/tasks.cpp
+++
@@ -1,9 +0,0 @@
-#include "tasks.hpp"
-
-void OpenIrisTasks::ScheduleRestart(int milliseconds) {
-    int initialTime = millis(); 
-    while (millis() - initialTime <= milliseconds) {
-        continue;
-    }
-    ESP.restart();
-}
